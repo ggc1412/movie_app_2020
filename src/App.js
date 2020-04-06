1ce46f1719a3c797fcd51aa288e8c9fac66659e3
ggc1412 <ggc1412@gmail.com>
2020-04-06 23:51:16 +0900

    :heavy_plus_sign:first Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import First from "./First";
 
 function App() {
   return (
     <div>
       <h2>Hello</h2>
+      <First />
     </div>
   );
 }
---
+++ src/First.js
@@ -0,0 +1,10 @@
+import React from "react";
+// JSX를 사용하기 위해 React를 import 한다.
+// JSX는 javascript 안의 HTML 형태
+// React에서 Component를 사용해서 HTML를 작성하기 위해 사용한다.
+
+function First() {
+    return <h3>It is first Component</h3>;
+}
+
+export default First;
