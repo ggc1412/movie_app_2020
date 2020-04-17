c97b22112ad681da483ddd8088fa82e15d7ca4b9
ggc1412 <ggc1412@gmail.com>
2020-04-17 23:40:51 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component{
     state = {
-        count: 0
+        isLoading: true,
+        Movie: []
+    };
+
+    Loading = () => {
+        this.setState({
+            isLoading: false
+        })
     };
     
-    add = () => {
-        this.setState( current => ({
-            count: current.count + 1
-        }))
-    };
-
-    minus = () => {
-        this.setState( current => ({
-            count: current.count - 1
-        }))
+    componentDidMount = () =>{
+        setInterval(this.Loading,5000)
     }
+    // render에 이 setInterval 메서드를 넣게 되면 re-render되면서 setInterval 메서드가 다시 실행된다.
+    // componentDidMount에 메서드를 넣으면 reder와는 상관 없기 때문에 ( re-render는 업데이트임 ) 한번만 실행된다.   
 
     render(){
+        const { isLoading } = this.state;
+        
         return (
             <div>
-                <h1>Number: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
+                <h1>{isLoading ? "Loading..." : "We're ready!" }</h1>
             </div>
         );
     }
